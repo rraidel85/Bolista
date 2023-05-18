f8c821c020070fc8884529499899babd76d6d323
Pedro Luis <pedroluis200@nauta.com.cu>
2023-05-18 01:26:14 -0400

    Create the menu of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ApplicationRef } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -6,13 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Inicio', url: '/folder/Inicio', icon: 'home' },
+    { title: 'Pase', url: '/folder/Pase', icon: 'document' },
+    { title: 'Pase +', url: '/folder/Pase +', icon: 'documents' },
+    { title: 'Ganadores', url: '/folder/Ganadores', icon: 'calendar' },
+    { title: 'Premios', url: '/folder/Premios', icon: 'trophy' },
+    { title: 'Configuración', url: '/folder/Configuración', icon: 'settings' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  
+  constructor(private appRef: ApplicationRef) {
+  }
+
+  close() {
+    // Close the application
+    this.appRef.destroy();
+  
+  }
 }
