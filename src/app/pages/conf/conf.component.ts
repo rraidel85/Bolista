62fa09562325b0dd3fed92220a208b26ce6ec7a9
Pedro Luis <pedroluis200@nauta.com.cu>
2023-05-18 15:58:06 -0400

    Add some options to conf page and start doing inicio cards

--- src/app/pages/conf/conf.component.ts
+++ src/app/pages/conf/conf.component.ts
@@ -1,13 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-conf',
   templateUrl: './conf.component.html',
   styleUrls: ['./conf.component.scss'],
 })
+
+@Injectable({
+  providedIn: 'root'
+})
+
 export class ConfComponent  implements OnInit {
 
-  constructor() { }
+  public darkMode:boolean = false;
+
+  constructor() {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: light)');
+    this.darkMode = prefersDark.matches;
+   }
+
+
+   cambio(){
+    //const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+    this.darkMode = !this.darkMode;
+    document.body.classList.toggle('dark');
+   }
 
   ngOnInit() {}
 
