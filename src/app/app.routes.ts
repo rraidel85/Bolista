7a8f4deab91dd40ccc3c9f40a240d603240e4c2a
Raidel <rraidel85@gmail.com>
2023-05-28 21:58:28 -0700

    change route names to lowercase

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,15 @@ import {  Routes } from '@angular/router';
 export const APP_ROUTES: Routes = [
   {
     path: '',
-    redirectTo: 'Lista',
+    redirectTo: 'lista',
     pathMatch: 'full'
   },
   {
-    path: 'Lista',
-    loadChildren: () =>import('./pages//lista/list.routes').then(m => m.LIST_ROUTES)
+    path: 'lista',
+    loadChildren: () =>import('./pages/lista/list.routes').then(m => m.LIST_ROUTES)
   },
   {
-    path: 'Configuración',
+    path: 'configuracion',
     loadComponent: () =>import('./pages/conf/conf.component').then(c => c.ConfComponent)
   }
   
