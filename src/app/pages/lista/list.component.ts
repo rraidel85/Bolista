063a74aeca86c0b010af951c4b85cf5f7dc6a386
Pedro Luis <pedroluis200@nauta.com.cu>
2023-06-07 23:30:04 -0400

    Fix hora pipe in list view

--- src/app/pages/lista/list.component.ts
+++ src/app/pages/lista/list.component.ts
@@ -4,42 +4,49 @@ import { DayCardComponent } from './components/day-card/day-card.component';
 import { RouterLink } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { HoraService } from 'src/app/services/hora.service';
+import { HoraPipe } from 'src/app/pipes/hora.pipe';
 
 @Component({
   selector: 'app-inicio',
   template: `
-    <ion-header [translucent]="true">
-      <ion-toolbar>
-        <ion-buttons slot="start">
-          <ion-menu-button></ion-menu-button>
-        </ion-buttons>
-        <ion-title>Lista</ion-title>
-      </ion-toolbar>
-    </ion-header>
+   <ion-header [translucent]="true">
+  <ion-toolbar>
+    <ion-buttons slot="start">
+      <ion-menu-button></ion-menu-button>
+    </ion-buttons>
+    <ion-title>Lista</ion-title>
+    <ion-text class="hour" slot="end">{{horaActual|hora}}</ion-text>
+    
+  </ion-toolbar>
+</ion-header>
 
-    <ion-content [fullscreen]="true">
-      <ion-item id="list-buttons" lines="none">
-        <ion-label>
-          <ion-grid>
-            <div class="card-mid" routerLink="">
-              <div class="buttons">
-                <ion-button style="width: 99px">Cuadres</ion-button>
-                <ion-button>Patrones</ion-button>
-              </div>
+<ion-content [fullscreen]="true">
+  <ion-item id="list-buttons" lines="none">
+    <ion-label>
+      <ion-grid>
+                
+        <div class="card-mid" routerLink="">
+           <div class="buttons">
+            <ion-button style="width: 99px;">Cuadres</ion-button>
+            <ion-button>Patrones</ion-button>
+           </div>
 
-              <div class="buttons">
-                <ion-button>Patrones</ion-button>
-                <ion-button>Patrones</ion-button>
-              </div>
+           <div class="buttons"> 
+            <ion-button>Patrones</ion-button>
+            <ion-button>Patrones</ion-button>
+           </div>
 
-              <div class="buttons">
-                <ion-button>Patrones</ion-button>
-                <ion-button>Patrones</ion-button>
-              </div>
-            </div>
-          </ion-grid>
-        </ion-label>
-      </ion-item>
+           <div class="buttons">
+            <ion-button>Patrones</ion-button>
+            <ion-button>Patrones</ion-button>
+          </div>
+        </div>
+  
+       
+                
+      </ion-grid>
+    </ion-label>
+  </ion-item>
 
       <app-day-card [routerLink]="['contactos']"></app-day-card>
       <app-night-card [routerLink]="[]"></app-night-card>
@@ -48,7 +55,7 @@ import { HoraService } from 'src/app/services/hora.service';
   `,
   styleUrls: ['./list.component.scss'],
   standalone: true,
-  imports: [IonicModule, RouterLink, DayCardComponent, NightCardComponent],
+  imports: [IonicModule, RouterLink,HoraPipe, DayCardComponent, NightCardComponent],
 })
 export class ListComponent  implements OnInit {
 
