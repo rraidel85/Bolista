f9c4d0461b30e6e8a2a4d03270bcebffdb8e144b
Pedro Luis <pedroluis200@nauta.com.cu>
2023-06-08 23:10:16 -0400

    Add hora service in conf view

--- src/app/pages/conf/conf.component.ts
+++ src/app/pages/conf/conf.component.ts
@@ -3,6 +3,7 @@ import { ModalController, IonicModule } from '@ionic/angular';
 import { PayModalComponent } from './components/pay-modal/pay-modal.component';
 import { LimitModalComponent } from './components/limit-modal/limit-modal.component';
 import { HoraService } from 'src/app/services/hora.service';
+import { HoraPipe } from 'src/app/pipes/hora.pipe';
 
 @Component({
   selector: 'app-conf',
@@ -13,6 +14,7 @@ import { HoraService } from 'src/app/services/hora.service';
           <ion-menu-button></ion-menu-button>
         </ion-buttons>
         <ion-title>Configuración</ion-title>
+        <ion-text class="hour" slot="end">{{ horaActual | hora }}</ion-text>
       </ion-toolbar>
     </ion-header>
 
@@ -44,7 +46,7 @@ import { HoraService } from 'src/app/services/hora.service';
   `,
   styleUrls: ['./conf.component.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, HoraPipe],
 })
 
 export class ConfComponent  implements OnInit {
