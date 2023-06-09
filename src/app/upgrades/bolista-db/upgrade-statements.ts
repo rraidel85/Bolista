233a71d41faef26996b06fdd623acce147772aae
Jorge <jorgearencibia123456@gmail.com>
2023-06-09 08:45:59 -0400

    add limits and config table

--- src/app/upgrades/bolista-db/upgrade-statements.ts
+++ src/app/upgrades/bolista-db/upgrade-statements.ts
@@ -7,10 +7,30 @@ export const bolistaDbVersionUpgrades = [
           pick varchar(20) NOT NULL UNIQUE,
           price integer NOT NULL,
           amount integer NOT NULL,
+          grupo integer NOT NULL,
           corrido integer,
-          pase integer,
-          grupo integer
+          pase integer
         );`
+    ]
+  },
+  {
+    toVersion: 2,
+    statements: [
+      `CREATE TABLE IF NOT EXISTS limits (
+          id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+          pick varchar(20) NOT NULL UNIQUE,
+          grupo integer NOT NULL
+        );`,
+      `CREATE TABLE IF NOT EXISTS config (
+          id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+          pick3 integer NOT NULL UNIQUE,
+          pick4 integer NOT NULL UNIQUE,
+          limitado integer NOT NULL UNIQUE,
+          centena integer NOT NULL UNIQUE,
+          parle integer NOT NULL UNIQUE,
+          candado integer NOT NULL UNIQUE,
+          centena_c integer NOT NULL UNIQUE
+        );`,
     ]
   }
 ];
