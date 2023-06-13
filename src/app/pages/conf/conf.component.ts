e34daf7378e9ac86caec0fc2b2d7fd4d16835a53
Raidel <rraidel85@gmail.com>
2023-06-13 02:36:18 -0700

    Change theme init logic in conf page to constructor

--- src/app/pages/conf/conf.component.ts
+++ src/app/pages/conf/conf.component.ts
@@ -62,6 +62,16 @@ export class ConfComponent implements OnInit {
   ) {
     const prefersDark = window.matchMedia('(prefers-color-scheme: light)');
     this.darkMode = prefersDark.matches;
+    this.dbService.mDb.query(`select tema from config`).then((ret) => {
+      const tema = ret.values![0].tema;
+      if (tema === 'dark') {
+        this.darkMode = true;
+        document.body.classList.add('dark');
+      } else {
+        this.darkMode = false;
+        document.body.classList.remove('dark');
+      }
+    });
   }
 
   cambio() {
@@ -93,15 +103,5 @@ export class ConfComponent implements OnInit {
     this.horaService
       .obtenerHoraActual()
       .subscribe((hora) => (this.horaActual = hora));
-    this.dbService.mDb.query(`select tema from config`).then((ret) => {
-      const tema = ret.values![0].tema;
-      if (tema === 'dark') {
-        this.darkMode = true;
-        document.body.classList.add('dark');
-      } else {
-        this.darkMode = false;
-        document.body.classList.remove('dark');
-      }
-    });
   }
 }
