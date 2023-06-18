8eafa671a87e1d7468b3f9bcbab6d1825e22907f
Pedro Luis <pedroluis200@nauta.com.cu>
2023-06-17 22:12:56 -0400

    Fix hora pipe

--- src/app/pipes/hora.pipe.ts
+++ src/app/pipes/hora.pipe.ts
@@ -12,7 +12,7 @@ export class HoraPipe implements PipeTransform {
     const amPm = +horas >= 12 ? 'pm' : 'am';
     const hora12h = +horas % 12 || 12;
 
-    return `${hora12h}:${minutos}:${segundos} ${amPm}`;
+    return `${hora12h}:${minutos}.${segundos} ${amPm}`;
   }
   return ''
   }
